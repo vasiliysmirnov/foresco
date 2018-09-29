3b1b5b13b50595b77761415e50f0f0eae39078da
Василий <vasya_ves@icloud.com>
2018-09-29 12:37:53 +0300

    add sohobook template

---
+++ pages/projects/sohobook.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="container">
+            <app-header/>
+            <app-menu/>
+            <div class="content">
+                <section class="top">
+
+                </section>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+    import AppHeader from '~/components/header.vue'
+    import AppMenu from '~/components/menu.vue'
+    import AppFooter from '~/components/footer.vue'
+
+    export default {
+        components: {
+            AppHeader,
+            AppMenu,
+            AppFooter
+        },
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
