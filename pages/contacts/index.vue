fb27fef39392d1278b8b92f7e5d6817b355b3383
Василий <vasya_ves@icloud.com>
2018-10-14 17:10:31 +0300

    add form validation

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -10,25 +10,51 @@
                         <div class="address" v-for="contact in contacts" :key="contact.id">{{contact.address}}</div>
                     </div>
                     <div class="form">
-                        <form action="">
-                            <input type="text" name="name" placeholder="Ваше имя">
-                            <input type="phone" name="phone" placeholder="Ваш телефон">
+                        <form @submit="checkForm">
+                            <div class="form__item">
+                                <input type="text" name="name" id="form-name" v-model="name" :class="{active: name !== ''}">
+                                <label for="form-name">Ваше имя</label>
+                                <div class="error" v-if="nameErr">
+                                    {{nameErr}}
+                                </div>
+                            </div>
+                            <div class="form__item">
+                                <input type="phone" name="phone" id="form-phone" v-model="phone" :class="{active: phone !== ''}">
+                                <label for="form-phone">Ваш телефон</label>
+                                <div class="error" v-if="phoneErr">
+                                    {{phoneErr}}
+                                </div>
+                            </div>
+                            
                             <div class="radio">
-                                <div class="title">Тип проекта</div>
+                                
+                                <div class="title">
+                                    Тип проекта
+                                    <div class="error" v-if="typeProjectErr">
+                                        {{typeProjectErr}}
+                                    </div>
+                                </div>
+                                
                                 <div class="radio__item">
-                                    <input type="radio" name="typeProject" id="ios" value="ios">
+                                    <input type="radio" name="typeProject" id="ios" value="ios" v-model="typeProject">
                                     <label for="ios">IOS</label>
                                 </div>
                                 <div class="radio__item">
-                                    <input type="radio" name="typeProject" id="android" value="android">
+                                    <input type="radio" name="typeProject" id="android" value="android" v-model="typeProject">
                                     <label for="android">Android</label>
                                 </div>
                                 <div class="radio__item">
-                                    <input type="radio" name="typeProject" id="web" value="web">
+                                    <input type="radio" name="typeProject" id="web" value="web" v-model="typeProject">
                                     <label for="web">WEB</label>
                                 </div>
+                                
                             </div>
-                            <textarea name="comment" rows="3" placeholder="Ваш комментарий"></textarea>
+                            
+                            <div class="form__item">
+                                <textarea name="comment" rows="3" id="form-comment" v-model="comment" :class="{active: comment !== ''}"></textarea>
+                                <label for="form-comment">Ваш комментарий</label>
+                            </div>
+                            
                             <input type="submit" value="Заказать проект">
                         </form>
                     </div>
@@ -58,6 +84,49 @@ export default {
         contacts() {
             return this.$store.state.contacts
         }
+    },
+    data: function(){
+        return {
+            name: '',
+            nameErr: '',
+            phone: '',
+            phoneErr: '',
+            typeProject: '',
+            typeProjectErr: '',
+            comment: ''
+        }
+    },
+    methods:{
+        checkForm: function (e) {
+            if (this.name && this.phone) {
+                return true;
+            }
+
+            this.nameErr = '';
+            this.phoneErr = '';
+            this.typeProjectErr = ''
+
+            if (!this.name) {
+                this.nameErr = 'Введите ваше имя.';
+            }
+            if (!this.phone) {
+                this.phoneErr = 'Введите ваш телефон.';
+            } else if (!this.validPhone(this.phone)) {
+                this.phoneErr = 'Неверный формат телефона.';
+            }
+            if (!this.typeProject) {
+                this.typeProjectErr = 'Выберете тип проекта.';
+            }
+
+            e.preventDefault();
+        },
+        validPhone: function(phone){
+            var re = /^(\+{0,1})([0-9]{1})( |\-)?([0-9]{3})( |\-)?([0-9]{3})( |\-)?([0-9]{2})( |\-)?([0-9]{2})\b/;
+            return re.test(phone);
+        }
+    },
+    mounted () {
+
     }
 }
 </script>
@@ -100,6 +169,22 @@ export default {
             @media(max-width 768px)
                 width 100%
                 margin-bottom 50px
+            .form__item
+                position relative
+                margin 0 0 30px 0
+                label
+                    font-family: 'montserratregular'
+                    color rgba(255, 255, 255, .5)
+                    position absolute
+                    top 0
+                    left 0
+                    font-size 14px
+                    transform: translateY(7px);
+                    pointer-events: none
+                    transition: all .1s
+                .error
+                    color red
+                    font-size: 12px
             input[type=text],
             input[type=phone]
                 font-family: 'montserratregular'
@@ -110,8 +195,21 @@ export default {
                 width 100%
                 display block
                 padding 10px 0
-                margin 0 0 30px 0
+                margin 0
+                transition: all .1s
                 outline none
+                &:focus,
+                &.active
+                    border-color #e19d45
+                &:-webkit-autofill + label
+                    transform: translateY(-5px)
+                    font-size: 10px
+                    color #e19d45
+                &:focus + label
+                &.active + label
+                    transform: translateY(-5px)
+                    font-size: 10px
+                    color #e19d45
             textarea
                 font-family: 'montserratregular'
                 color rgba(255, 255, 255, .5)
@@ -123,6 +221,18 @@ export default {
                 padding 10px 0
                 margin 0 0 30px 0
                 outline none
+                &:focus,
+                &.active
+                    border-color #e19d45
+                &:-webkit-autofill + label
+                    transform: translateY(-5px)
+                    font-size: 10px
+                    color #e19d45
+                &:focus + label
+                &.active + label
+                    transform: translateY(-5px)
+                    font-size: 10px
+                    color #e19d45
             input[type='submit']
                 width: 100%
                 max-width: 321px
@@ -183,5 +293,8 @@ export default {
                         &:checked ~ label
                             background: rgba(255, 255, 255, 0.05)
                             border-color: #e19d45
+                .error
+                    color red
+                    font-size: 12px
 </style>
 
