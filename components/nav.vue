7044f44e82578bcf69178c44d31d160906146954
Vasiliy <ieatmybrain@gmail.com>
2019-05-30 16:04:15 +0300

    some fixes for hamburger

--- components/nav.vue
+++ components/nav.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="nav">
         <div class="nav__btn" @click='changeNavState' v-bind:class='{ active: showNav }'>
-            <span></span>
-            <span></span>
+            <div class="nav__inner">
+                <span></span>
+                <span></span>
+            </div>
         </div>
     </div>
 </template>
@@ -14,33 +16,43 @@
         span
             background: #272625
 .nav
+    &__inner
+        position absolute
+        top 50%
+        left calc(50% - 12px)
+        width: rem(24px)
+        height: rem(14px)
+        margin-top: -7px
     &__btn
         width: rem(50px)
         height: rem(50px)
         background: rgba(216, 216, 216, .05)
         cursor pointer
         position relative
-        padding: rem(19px 0 0 0)
         z-index 100
         &.active
             span
-                background: #eeeeee
+                top: calc(50% - 1px)
                 transform: rotate(45deg)
-                left: 0
-                top: rem(6px)
+                transition top .2s ease, transform .2s ease .2s
                 &:last-child
-                    transform: rotate(-45deg)
-                    top: rem(-6px)
-                    left: 0
+                    transition bottom .2s ease, transform .2s ease .2s
+                    bottom: calc(50% - 1px);
+                    transform: rotate(-45deg);
         span
             width: rem(23px)
             height: rem(2px)
             border-radius: rem(1px)
             background: #eeeeee
             display: block
-            margin: 0 auto rem(10px)
-            position relative
-            transition all .2s ease
+            position absolute
+            top 0
+            left 0
+            transition top .2s ease .2s, bottom .2s ease .2s, transform .2s ease
+            &:last-child
+                margin-bottom 0
+                bottom 0
+                top: initial
 
 
 </style>
